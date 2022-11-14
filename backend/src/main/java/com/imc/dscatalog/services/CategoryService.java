4e9f00f09be55dffbfc20bd8d138c99d11d9d6d0
vitor <vitorcc13@gmail.com>
2022-11-14 15:53:56 -0300

    Database seeding

--- backend/src/main/java/com/imc/dscatalog/services/CategoryService.java
+++ backend/src/main/java/com/imc/dscatalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import com.imc.dscatalog.entities.Category;
 import com.imc.dscatalog.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -25,6 +26,13 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    /*
+        @Transactional iremos garantir que ou tudo seja executado, ou nada.
+        O readOnly serve
+        para evitar locking no banco, ou seja, não iremos travar o
+        banco só pra ler, é uma forma de melhorar a peformance.
+     */
+    @Transactional(readOnly = true)
     public List<Category> findAll(){
         return categoryRepository.findAll();
     }
