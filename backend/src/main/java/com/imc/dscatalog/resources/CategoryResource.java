db39d015eaf6872636a02f8189d9c26f20e2302b
vitor <vitorcc13@gmail.com>
2022-11-15 18:08:29 -0300

    pagination

--- backend/src/main/java/com/imc/dscatalog/resources/CategoryResource.java
+++ backend/src/main/java/com/imc/dscatalog/resources/CategoryResource.java
@@ -3,6 +3,9 @@ package com.imc.dscatalog.resources;
 import com.imc.dscatalog.dto.CategoryDTO;
 import com.imc.dscatalog.services.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -18,8 +21,14 @@ public class CategoryResource {
     private CategoryService categoryService;
 
     @GetMapping
-    public ResponseEntity<List<CategoryDTO>> findAll(){
-        List<CategoryDTO> categories = categoryService.findAll();
+    public ResponseEntity<Page<CategoryDTO>> findAll(
+            @RequestParam(value = "page", defaultValue = "0") Integer page,
+            @RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+            @RequestParam(value = "orderBy", defaultValue = "name") String orderBy
+    ){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Sort.Direction.valueOf(direction), orderBy);
+        Page<CategoryDTO> categories = categoryService.findAllPaged(pageRequest);
         return ResponseEntity.ok().body(categories);
     }
     @GetMapping(value = "/{id}")
