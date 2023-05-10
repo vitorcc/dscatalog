6b35854825c091719e86e3a026a88e08432ec705
vitor <vitorcc13@gmail.com>
2023-05-10 20:42:07 -0300

    Implement first integration test on product service and product reposity

---
+++ backend/src/test/java/com/imc/dscatalog/services/ProductServiceIT.java
@@ -0,0 +1,45 @@
+package com.imc.dscatalog.services;
+
+import com.imc.dscatalog.repositories.ProductRepository;
+import com.imc.dscatalog.services.exceptions.ResourceNotFoundException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProductServiceIT {
+
+    @Autowired
+    private ProductService service;
+
+    @Autowired
+    private ProductRepository repository;
+
+    private Long existingId;
+    private Long nonExistingId;
+    private Long countTotalProducts;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        existingId = 1L;
+        nonExistingId = 1000L;
+        countTotalProducts = 25L;
+    }
+
+    @Test
+    void deleteShouldDeleteResourceWhenIdExists() {
+        service.delete(existingId);
+        Assertions.assertEquals(countTotalProducts - 1, repository.count());
+    }
+
+    @Test
+    void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists(){
+        Assertions.assertThrows(ResourceNotFoundException.class, () -> {
+           service.delete(nonExistingId);
+        });
+    }
+
+
+}
